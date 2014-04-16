ad6e6c81f65e9163b9122889c1233ca215df1fb5
Gabrielle Watson <gabrielle.g.watson@gmail.com>
2014-04-16 11:06:30 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -20,3 +20,14 @@ class AddFriendForm(Form):
     category = SelectField(u'Category', choices=[('Relative','Relative'),
                                                  ('School','School'),
                                                  ('Work','Work')])
+class SignupForm(Form):
+     fname = TextField(u'Firstname', validators=[validators.Required()])
+     lname = TextField(u'Lastname', validators=[validators.Required()])
+     mar_stat = TextField(u'Mar_stat', choices=[('Married', 'Married'),
+                                                       ('Single','Single'),
+                                                       ('Divorced', 'Divorced'),
+                                                       ('Widowed', 'Widowed')])
+     email = TextField(u'email', validators=[validators.Required()])
+     password = PasswordField(u'Password', validators=[validators.Required()])
+     
+ 
